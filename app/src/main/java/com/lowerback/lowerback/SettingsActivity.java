8e2f42475c75728b0878a2d5ed0565a349998e8f
Marcelo_Miotto <mmiotto@gmail.com>
2016-03-09 14:53:57 -0300

    Version 1.1

--- app/src/main/java/com/lowerback/lowerback/SettingsActivity.java
+++ app/src/main/java/com/lowerback/lowerback/SettingsActivity.java
@@ -4,21 +4,18 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.Toast;
 
-public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
 
     EditText mAlarmIntervalEditText;
     private  Toolbar toolbar;
     String   mDefaultMusic;
     Button   mSaveButton;
-    Button   mResetButton;
-    Spinner  mMusicSpinner;
 
 
     @Override
@@ -41,18 +38,14 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
 
         mAlarmIntervalEditText = (EditText) findViewById(R.id.alarm_interval_EditText);
         mSaveButton   = (Button)   findViewById(R.id.save_id);
-        mResetButton  = (Button)   findViewById(R.id.reset_id);
-        mMusicSpinner = (Spinner)  findViewById(R.id.music_spinner);
 
         mSaveButton.setOnClickListener(this);
-        mResetButton.setOnClickListener(this);
         final SharedPreferences sharedPref = this.getSharedPreferences(
                 this.getString(R.string.alarm_interval_key_pref), this.MODE_PRIVATE);
         String alarmIntervalPref  = sharedPref.getString(this.getString(R.string.alarm_interval_alarm_interval_pref)
                 , this.getString(R.string.alarm_interval_default));
 
         mAlarmIntervalEditText.setText(alarmIntervalPref);
-
 
     }
 
@@ -65,21 +58,27 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
 
         switch (v.getId()){
             case R.id.save_id:
-                editor.putString(getString(R.string.alarm_music_pref),String.valueOf(mMusicSpinner.getSelectedItem()));
                 editor.putString(getString(R.string.alarm_interval_alarm_interval_pref),  mAlarmIntervalEditText.getText().toString());
                 editor.commit();
                 Toast.makeText(SettingsActivity.this,getResources().getText(R.string.alarm_interval_saved), Toast.LENGTH_SHORT).show();
                 break;
 
-            case R.id.reset_id:
-                mDefaultMusic = "android.resource://"+ this.getPackageName() + "/" + R.raw.kalimba;
-                Log.i("Debug","path: "+mDefaultMusic);
-                mAlarmIntervalEditText.setText(getString(R.string.alarm_interval_default));
-                mMusicSpinner.setSelection(1);
-                Toast.makeText(SettingsActivity.this,getResources().getText(R.string.default_alarm_values), Toast.LENGTH_SHORT).show();
-                break;
-
         }
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
